cb921e26b5c20b85a60a12c8bfe30f87f3fcfcfc
Andrés Rojas <anrj3099@hotmail.com>
2019-08-03 16:29:47 -0400

    Proyecto final sin modificaciones.
    -Los modelos están sin cambios.
    
    -Los controladoras están sin cambios.
    
    -Las Vistas están sin cambios.
    
    --Esta es la línea base del código fuente.

---
+++ LibrariaManagement/LibrariaManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibrariaManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
