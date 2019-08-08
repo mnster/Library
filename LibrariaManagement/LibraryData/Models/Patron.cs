2b46ea5c73bf2b82b5c90ba6b53cdb451ff3f82a
Andrés Rojas <anrj3099@hotmail.com>
2019-08-08 18:09:02 -0400

    -Entity Framework (Models, Migrations) -Actualización del DB

--- LibrariaManagement/LibraryData/Models/Patron.cs
+++ LibrariaManagement/LibraryData/Models/Patron.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LibraryData.Models
@@ -7,12 +8,23 @@ namespace LibraryData.Models
     public class Patron
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public DateTime  DateOfBirth { get; set; }
-        public string PhoneNummber { get; set; }
 
-        //public virtual LibraryCard LibraryCard { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Address { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
     }
 }
