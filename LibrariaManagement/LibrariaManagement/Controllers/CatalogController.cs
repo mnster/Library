661a854abfd703ac249c45f6df0bf9de1da4e3c4
Andrés Rojas <anrj3099@hotmail.com>
2019-08-09 02:14:19 -0400

    -MVC (Controller, Models, Views) -Data -Services

--- LibrariaManagement/LibrariaManagement/Controllers/CatalogController.cs
+++ LibrariaManagement/LibrariaManagement/Controllers/CatalogController.cs
@@ -1,4 +1,6 @@
-﻿using LibraryData;
+﻿using System.Linq;
+using LibrariaManagement.Models.Catalog;
+using LibraryData;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibrariaManagement.Controllers
@@ -15,6 +17,46 @@ namespace LibrariaManagement.Controllers
         public IActionResult Index()
         {
             var assetModels = _assets.GetAll();
+
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    Title = result.Title,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Type = _assets.GetType(result.Id)
+                });
+
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+
+            return View(model);
+
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var model = new AssetDetailModel
+            {
+                AsssetId = asset.Id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(id),
+                Type = _assets.GetType(id),
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                Isbn = _assets.GetIsbn(id)
+            };
+
+            return View(model);
         }
     }
 }
